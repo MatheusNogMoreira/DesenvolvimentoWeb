c77991ef8dd46c1e397bfd175881ca93ca1e12ca
Matheus Nogueira Moreira <90008448+MatheusNogMoreira@users.noreply.github.com>
2025-05-21 21:31:07 -0300

    Aula sobre variáveis em javascript

---
+++ Geral/JavaScript/Variaveis/script.js
@@ -0,0 +1,23 @@
+//string number(int+float) boolean
+//palavra chave 'var'
+
+//string 
+//palavra chave | identificador | atribuição | conteúdo(determina o tipo da variável)
+var exemplo_string = 'Matheus Nogueira'
+
+//number
+var exemplo_number = -7.5
+
+//booleano
+var exemplo_boolean = true
+
+//Para debug
+//alert
+//abrir um dialog
+alert(exemplo_string)
+
+//escreve no body
+document.write(exemplo_boolean)
+
+console.log(exemplo_number)
+
